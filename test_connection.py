c4afd1d621092babed34db5b97e222da0f461623
sharnik24 <165623877+sharnik24@users.noreply.github.com>
2025-12-14 03:25:56 +0400

    Update test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -1,17 +1,2 @@
-# test_connection.py
-from my_utils import get_sheet  # Import the function from my_utils.py
-
-sheet = get_sheet()
-
-# Get all values from the sheet (includes headers)
-data = sheet.get_all_values()
-
-if not data:
-    print("No data found in the sheet!")
-else:
-    print("Data from the sheet:\n")
-
-# Loop through rows and skip empty rows
-for row in data:
-    if any(cell.strip() for cell in row):  # Skip completely empty rows
-        print(row)
+from my_utils import get_companies
+print(get_companies())
