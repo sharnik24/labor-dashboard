3e91a6e9abcde19d37a8bf21cebd870ff6d99139
sharnik24 <165623877+sharnik24@users.noreply.github.com>
2025-12-13 21:16:39 +0400

    Update my_utils.py

--- my_utils.py
+++ my_utils.py
@@ -1,36 +1,32 @@
-# my_utils.py
 import gspread
-from google.oauth2.service_account import Credentials
-from datetime import datetime, timedelta
+from oauth2client.service_account import ServiceAccountCredentials
 
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
+# Connect to Google Sheet
+def connect_sheet():
+    scope = ["https://spreadsheets.google.com/feeds",
+             "https://www.googleapis.com/auth/spreadsheets",
+             "https://www.googleapis.com/auth/drive.file",
+             "https://www.googleapis.com/auth/drive"]
 
-SHEET_NAME = "labor_data"
+    creds = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
+    client = gspread.authorize(creds)
+    sheet = client.open("LaborData").sheet1  # Make sure your sheet name matches
+    return sheet
 
-def get_sheet():
-    creds = Credentials.from_service_account_file("/etc/secrets/credentials.json", scopes=SCOPES)
-    gc = gspread.authorize(creds)
-    sh = gc.open(SHEET_NAME)
-    worksheet = sh.sheet1
-    return worksheet
-
+# Get all employees
 def get_all_data():
-    worksheet = get_sheet()
-    data = worksheet.get_all_records()
-    return data
+    sheet = connect_sheet()
+    records = sheet.get_all_records()  # Returns a list of dicts
+    return records
 
-def get_expiring_soon(days=6):
-    data = get_all_data()
-    expiring = []
-    today = datetime.today().date()
-    for row in data:
-        expiry_str = row.get("Expiry Date")  # Column in your sheet
-        if expiry_str:
-            expiry_date = datetime.strptime(expiry_str, "%Y-%m-%d").date()
-            if 0 <= (expiry_date - today).days <= days:
-                expiring.append(row)
-    return expiring
-
+# Add new employee
+def add_employee(data):
+    sheet = connect_sheet()
+    sheet.append_row([
+        data["Company"],
+        data["Name"],
+        data["Position"],
+        data["Expiry"],
+        data["Email"],
+        data["WhatsAppNumber"]
+    ])
